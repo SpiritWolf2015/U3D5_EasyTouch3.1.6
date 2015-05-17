e5694de73ef08583011ee766e02fd3a071f3595b
SpiritWolf2015 <522414019@qq.com>
2015-05-18 00:36:15 +0800

    角色控制器1.1
    虚拟摇杆事件处理函数。

--- Assets/EasyTouch/Example/C# Example/Examples for EasyJoystick/Example-thirdPerson_DirectEventMode/JoystickEvent.cs
+++ Assets/EasyTouch/Example/C# Example/Examples for EasyJoystick/Example-thirdPerson_DirectEventMode/JoystickEvent.cs
@@ -12,37 +12,38 @@ public class JoystickEvent : MonoBehaviour {
         m_Animation = GetComponent<Animation>( );
     }
 
-	void OnEnable(){
-		EasyJoystick.On_JoystickMove += On_JoystickMove;
-		EasyJoystick.On_JoystickMoveEnd += On_JoystickMoveEnd;
-	}
-	
-	void OnDisable(){
-		EasyJoystick.On_JoystickMove -= On_JoystickMove	;
-		EasyJoystick.On_JoystickMoveEnd -= On_JoystickMoveEnd;
-	}
-		
-	void OnDestroy(){
-		EasyJoystick.On_JoystickMove -= On_JoystickMove;	
-		EasyJoystick.On_JoystickMoveEnd -= On_JoystickMoveEnd;
-	}
-	
-	
-	void On_JoystickMoveEnd(MovingJoystick move){
-		if (move.joystickName == "Move_Turn_Joystick"){
-			GetComponent<Animation>().CrossFade("idle");
-		}
-	}
-	void On_JoystickMove( MovingJoystick move){
-		if ("Move_Turn_Joystick" == move.joystickName == ){			
-			//
-			if (Mathf.Abs(move.joystickAxis.y)>0 && Mathf.Abs(move.joystickAxis.y)<0.5){
-                m_Animation.CrossFade("walk");				
-			}	
-			else if (Mathf.Abs(move.joystickAxis.y)>=0.5){
-                m_Animation.CrossFade("run");	
-			}
-		}
-	}
+    void OnEnable ( ) {
+        EasyJoystick.On_JoystickMove += On_JoystickMove;
+        EasyJoystick.On_JoystickMoveEnd += On_JoystickMoveEnd;
+    }
+
+    void OnDisable ( ) {
+        EasyJoystick.On_JoystickMove -= On_JoystickMove;
+        EasyJoystick.On_JoystickMoveEnd -= On_JoystickMoveEnd;
+    }
+
+    void OnDestroy ( ) {
+        EasyJoystick.On_JoystickMove -= On_JoystickMove;
+        EasyJoystick.On_JoystickMoveEnd -= On_JoystickMoveEnd;
+    }
+
+    // 虚拟摇杆事件处理
+
+    void On_JoystickMoveEnd ( MovingJoystick move ) {
+        if ( move.joystickName == "Move_Turn_Joystick" ) {
+            GetComponent<Animation>( ).CrossFade("idle");
+        }
+    }
+
+    void On_JoystickMove ( MovingJoystick move ) {
+        if ( "Move_Turn_Joystick" == move.joystickName ) {
+            //
+            if ( Mathf.Abs(move.joystickAxis.y) > 0 && Mathf.Abs(move.joystickAxis.y) < 0.5 ) {
+                m_Animation.CrossFade("walk");
+            } else if ( Mathf.Abs(move.joystickAxis.y) >= 0.5 ) {
+                m_Animation.CrossFade("run");
+            }
+        }
+    }
 
 }
